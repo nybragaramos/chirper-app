d26d1335252af27ed68de6b4cc22427a86ad7d09
nadjayanna <nadjayannabr@gmail.com>
2018-12-20 16:04:58 +0100

    feat: add loading feature to app

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,19 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Dashboard/>
+      	{this.props.loading === true 
+      		? null 
+      		: <Dashboard/>
+      	}
       </div>
     )
   }
 }
 
-export default connect()(App)
+function mapStateToProps({ authedUser }) {
+	return {
+		loading: authedUser === null
+	}
+}
+
+export default connect(mapStateToProps)(App)
