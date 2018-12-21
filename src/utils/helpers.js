06ae2f305dda20258db12b490511538fde0b5b79
nadjayanna <nadjayannabr@gmail.com>
2018-12-21 13:40:16 +0100

    fix: change LocaleTime from US to DE

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,6 @@
 export function formatDate (timestamp) {
   const d = new Date(timestamp)
-  const time = d.toLocaleTimeString('en-US')
+  const time = d.toLocaleTimeString('de-DE')
   return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString()
 }
 
