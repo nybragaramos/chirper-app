8cbabd7b249e6b7a05c5ed1dee2e221e53e4f0dd
nadjayanna <nadjayannabr@gmail.com>
2018-12-24 14:46:29 +0100

    Updates

---
+++ precache-manifest.25109f124ce7d4fd814240ec29b05110.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55f023e3d724bb4a9f11",
+    "url": "/chirper-app/static/js/runtime~main.55f023e3.js"
+  },
+  {
+    "revision": "0bd8db63d8f79c44237c",
+    "url": "/chirper-app/static/js/main.0bd8db63.chunk.js"
+  },
+  {
+    "revision": "5b7471edd8729d5dd66a",
+    "url": "/chirper-app/static/js/1.5b7471ed.chunk.js"
+  },
+  {
+    "revision": "0bd8db63d8f79c44237c",
+    "url": "/chirper-app/static/css/main.80c837bd.chunk.css"
+  },
+  {
+    "revision": "1d5fd9087be0053d96c731c8a10bd8b6",
+    "url": "/chirper-app/index.html"
+  }
+];
